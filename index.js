88084335ef4051cca1658215df6771ac9023829a
Jerricke <eejerrickw@gmail.com>
2023-05-12 18:57:48 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+function increment(x) {
+    return ++x;
+}
+
+function decrement(x) {
+    return --x;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
